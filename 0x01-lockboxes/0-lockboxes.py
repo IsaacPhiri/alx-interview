1df2c11a1c85c28fb539cf258c5b5bd528c3a2d2
IsaacPhiri <isaacphiri315@gmail.com>
2023-03-31 02:07:28 +0000

    Fixed pep8 styling requirements

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -7,32 +7,32 @@ def canUnlockAll(boxes):
     Determines if all the boxes can be opened.
 
     Args:
-    boxes (list): A list of lists. 
+    boxes (list): A list of lists.
     Each list contains keys that can open other boxes.
 
     Returns:
     True if all boxes can be opened, else False.
     """
 
-    # Create a list of keys for available boxes, 
-    #initialized with the first key
+    # Create a list of keys for available boxes,
+    # initialized with the first key
     available_keys = [0]
 
     # Create a list to keep track of unlocked boxes,
-    #initialized with the first box
+    # initialized with the first box
     unlocked_boxes = [0]
 
-    # Iterate through the list of available keys 
-    #and unlock any boxes that correspond to the key
+    # Iterate through the list of available keys
+    # and unlock any boxes that correspond to the key
     while available_keys:
         key = available_keys.pop()
 
-        # Iterate through the list of keys in the 
-        #box that corresponds to the key
+        # Iterate through the list of keys in the
+        # box that corresponds to the key
         for box_key in boxes[key]:
 
-            # If the key is not already in the available_keys 
-            #or unlocked_boxes lists, add it to available_keys
+            # If the key is not already in the available_keys
+            # or unlocked_boxes lists, add it to available_keys
             if box_key not in available_keys and box_key not in \
                     unlocked_boxes and box_key < len(boxes):
                 available_keys.append(box_key)
