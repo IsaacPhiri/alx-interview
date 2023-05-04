8ef9e46724363bd2b3598a34082035977242843f
IsaacPhiri <isaacphiri315@gmail.com>
2023-05-04 02:59:23 +0000

    Api

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+const request = require('request');
+
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+
+request(apiUrl, function (error, response, body) {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  const charactersUrls = JSON.parse(body).characters;
+
+  charactersUrls.forEach(function (url) {
+    request(url, function (error, response, body) {
+      if (error) {
+        console.error(error);
+        return;
+      }
+
+      const characterName = JSON.parse(body).name;
+      console.log(characterName);
+    });
+  });
+});
