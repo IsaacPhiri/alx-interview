5b7e15c9a83423555083960109e7d3bd998b55da
Isaac Phiri <isaacphiri315@gmail.com>
2023-08-25 00:03:57 +0200

    Edits.

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -12,17 +12,17 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    rem = total
+    remainder = total
     coins_count = 0
-    coin_idx = 0
+    coin_index = 0
     sorted_coins = sorted(coins, reverse=True)
     n = len(coins)
-    while rem > 0:
-        if coin_idx >= n:
+    while remainder > 0:
+        if coin_index >= n:
             return -1
-        if rem - sorted_coins[coin_idx] >= 0:
-            rem -= sorted_coins[coin_idx]
+        if remainder - sorted_coins[coin_index] >= 0:
+            remainder -= sorted_coins[coin_index]
             coins_count += 1
         else:
-            coin_idx += 1
+            coin_index += 1
     return coins_count
