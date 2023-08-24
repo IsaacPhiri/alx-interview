c6ad8fd56ae95122518e4ed6d2d55f3c2d291125
Isaac Phiri <isaacphiri315@gmail.com>
2023-08-24 23:44:50 +0200

    Refactored the code to improve efficiency.

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -4,6 +4,7 @@ Making Change module
 """
 
 
+
 def makeChange(coins, total):
     """
     Given a pile of coins of different values, determine the fewest number of
@@ -12,12 +13,15 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0  # Base case
+    coins.sort(reverse=True)
+
+    dp = {}
+    dp[0] = 0
 
     for i in range(1, total + 1):
+        dp[i] = float('inf')
         for coin in coins:
-            if i >= coin:
+            if coin <= i:
                 dp[i] = min(dp[i], dp[i - coin] + 1)
 
     return dp[total] if dp[total] != float('inf') else -1
