a534f0e4bd37b84b67c067454c26e917b9481cc3
Isaac Phiri <isaacphiri315@gmail.com>
2023-08-24 23:56:36 +0200

    Refactored for efficiency.

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -13,15 +13,19 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    coins.sort(reverse=True)
+    rem = total
+    coins_count = 0
+    coin_idx = 0
+    
+    sorted_coins = sorted(coins, reverse=True)
 
-    dp = {}
-    dp[0] = 0
-
-    for i in range(1, total + 1):
-        dp[i] = float('inf')
-        for coin in coins:
-            if coin <= i:
-                dp[i] = min(dp[i], dp[i - coin] + 1)
-
-    return dp[total] if dp[total] != float('inf') else -1
+    n = len(coins)
+    while rem > 0:
+        if coin_idx >= n:
+            return -1
+        if rem - sorted_coins[coin_idx] >= 0:
+            rem -= sorted_coins[coin_idx]
+            coins_count += 1
+        else:
+            coin_idx += 1
+    return coins_count
